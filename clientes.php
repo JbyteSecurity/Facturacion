37d620ef4558028fa85f2de464925132b6d02ef3
jordan1988 <jordandiaz2016@gmail.com>
2018-03-24 12:13:33 -0500

    Cambio Cabecera

--- clientes.php
+++ clientes.php
@@ -1,8 +1,8 @@
 <?php
 	/*-------------------------
 	Autor: Jordan Diaz
-	Web: obedalvarado.pw
-	Mail: info@obedalvarado.pw
+	Web: 
+	Mail: jordandiaz@gmail.com
 	---------------------------*/
 	session_start();
 	if (!isset($_SESSION['user_login_status']) AND $_SESSION['user_login_status'] != 1) {
