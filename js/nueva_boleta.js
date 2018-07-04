72b2416be369ead155f29b66d3dfa3af446384e8
jordandiaz2016 <jordandiaz2016@gmail.com>
2018-07-04 12:36:55 -0500

    Se agrego que al crear un producto desde el ingreso de boleta se agrege directamente a la boleta

--- js/nueva_boleta.js
+++ js/nueva_boleta.js
@@ -50,7 +50,37 @@
 		}
 			});
 		}
-		
+		function agregarmas (id)
+		{
+			var precio_venta=document.getElementById('precio').value;
+			var cantidad=document.getElementById('cantidad').value;
+			//Inicia validacion
+			if (isNaN(cantidad))
+			{
+			alert('Esto no es un numero');
+			document.getElementById('cantidad').focus();
+			return false;
+			}
+			if (isNaN(precio_venta))
+			{
+			alert('Esto no es un numero');
+			document.getElementById('precio_venta').focus();
+			return false;
+			}
+			//Fin validacion
+			
+			$.ajax({
+        type: "POST",
+        url: "./ajax/agregar_boletas.php",
+        data: "id="+id+"&precio_venta="+precio_venta+"&cantidad="+cantidad,
+		 beforeSend: function(objeto){
+			$("#resultados").html("Mensaje: Cargando...");
+		  },
+        success: function(datos){
+		$("#resultados").html(datos);
+		}
+			});
+		}
 			function eliminar (id)
 		{
 			
